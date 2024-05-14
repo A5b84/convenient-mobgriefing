a13c1914a5133c39aabc029486c832e1fc95b11b
lukasabbe <67807954+lukasabbe@users.noreply.github.com>
2024-05-14 14:13:46 +0200

    1.20.6 support

--- src/main/java/io/github/a5b84/convenientmobgriefing/mixin/WorldMixin.java
+++ src/main/java/io/github/a5b84/convenientmobgriefing/mixin/WorldMixin.java
@@ -10,8 +10,7 @@ import static io.github.a5b84.convenientmobgriefing.Mod.createExplosionRuleOverr
 
 @Mixin(World.class)
 public abstract class WorldMixin {
-
-    @ModifyArg(method = "createExplosion(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lnet/minecraft/world/explosion/ExplosionBehavior;DDDFZLnet/minecraft/world/World$ExplosionSourceType;ZLnet/minecraft/particle/ParticleEffect;Lnet/minecraft/particle/ParticleEffect;Lnet/minecraft/sound/SoundEvent;)Lnet/minecraft/world/explosion/Explosion;", at = @At(value = "INVOKE", target = Targets.GET_RULE_BOOLEAN))
+    @ModifyArg(method = "createExplosion(Lnet/minecraft/entity/Entity;Lnet/minecraft/entity/damage/DamageSource;Lnet/minecraft/world/explosion/ExplosionBehavior;DDDFZLnet/minecraft/world/World$ExplosionSourceType;ZLnet/minecraft/particle/ParticleEffect;Lnet/minecraft/particle/ParticleEffect;Lnet/minecraft/registry/entry/RegistryEntry;)Lnet/minecraft/world/explosion/Explosion;", at = @At(value = "INVOKE", target = Targets.GET_RULE_BOOLEAN))
     private GameRules.Key<GameRules.BooleanRule> mobGriefingProxy(GameRules.Key<GameRules.BooleanRule> old) {
         return createExplosionRuleOverride != null
                 ? createExplosionRuleOverride
